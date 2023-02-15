dc94f97e55ee4d5f31c32a2018a4f46ee4ab4473
verawatisimatupang <88297371+verawatisimatupang@users.noreply.github.com>
2023-02-15 21:41:15 +0700

    Update RanesCipherBase.py

--- src/RanesCipherBase.py
+++ src/RanesCipherBase.py
@@ -433,7 +433,26 @@ class RanesCipherBasePage(Tk.Frame):
             ciphertext.append(formula)
         return ciphertext
     
-    # Encyrpt
+    # Encrypt
+    def encrypt(self):
+        plaintext = self.plain.get()
+        key = self.key.get()
+
+        if len(plaintext) == 0:
+            messagebox.showerror("Error", "Please input plaintext / file")
+        elif len(key) == 0:
+            messagebox.showerror("Error", "Please input key / file")
+        else :   
+            convert_to_string = self.base64_to_text(plaintext)
+            ksa_key = self.KSA(key)
+            prga_ksakey_plaintext = self.PRGA(ksa_key, convert_to_string)
+            xor_plaintext_prgaksakey = self.XOR(convert_to_string, prga_ksakey_plaintext)
+            result_string = self.ascii_to_text(xor_plaintext_prgaksakey)
+            result_base64 = self.text_to_base64(result_string)
+        self.cipher_string.set(result_string)
+        self.cipher_base.set(result_base64)
+
+    # Decyrpt
     def decrypt(self):
         plaintext = self.plain.get()
         key = self.key.get()
